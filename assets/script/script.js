6507f4704fc90e4b5358464ca6ee83115be175cb
mesmer <tchoupounathanael@gmail.com>
2023-07-08 12:55:54 +0100

    done some changes

--- assets/script/script.js
+++ assets/script/script.js
@@ -1,38 +1,39 @@
 const items = [
-  { name: 'book', weight: 8, value: 1 },
-  { name: 'pen', weight: 2, value: 2 },
-  { name: 'ruler', weight: 4, value: 3 },
-  { name: 'pencil', weight: 3, value: 4 },
-  { name: 'calculator', weight: 5, value: 5 },
-  { name: 'sweet', weight: 1, value: 6 },
-  { name: 'biscuit', weight: 3, value: 7 },
-  { name: 'bread', weight: 6, value: 8 },
-  { name: 'doughnut', weight: 7, value: 9 },
-  { name: 'cake', weight: 5, value: 1 },
-  { name: 'shoe', weight: 12, value: 5 },
-  { name: 'balloon', weight: 5, value: 3 },
-  { name: 'computer', weight: 50, value: 7 },
-  { name: 'iPhone', weight: 15, value: 1 },
-  { name: 'stopwatch', weight: 2, value: 10 },
-  { name: 't-shirt', weight: 4, value: 6 },
-  { name: 'coat', weight: 8, value: 8 },
-  { name: 'handkerchief', weight: 2, value: 1 },
-  { name: 'USB-key', weight: 3, value: 6 },
-  { name: 'smartphone', weight: 2, value: 3 }
+  { name: 'book', weight: 8, value: 200 },
+  { name: 'pen', weight: 2, value: 100 },
+  { name: 'ruler', weight: 4, value: 300 },
+  { name: 'pencil', weight: 3, value: 100 },
+  { name: 'calculator', weight: 5, value: 1000 },
+  { name: 'sweet', weight: 1, value: 50 },
+  { name: 'biscuit', weight: 3, value: 25 },
+  { name: 'bread', weight: 6, value: 100 },
+  { name: 'doughnut', weight: 7, value: 50 },
+  { name: 'cake', weight: 5, value: 125 },
+  { name: 'shoe', weight: 12, value: 5000 },
+  { name: 'balloon', weight: 5, value: 25 },
+  { name: 'computer', weight: 50, value: 100000 },
+  { name: 'iPhone', weight: 15, value: 300000 },
+  { name: 'stopwatch', weight: 2, value: 800 },
+  { name: 't-shirt', weight: 4, value: 2500 },
+  { name: 'coat', weight: 8, value: 15000 },
+  { name: 'handkerchief', weight: 2, value: 700 },
+  { name: 'USB-key', weight: 3, value: 5000 },
+  { name: 'smartphone', weight: 2, value: 75000 }
 ]
 
 const options = document.getElementById('select')
 const maxCapacity = document.querySelector('.capacity')
-const knapsackStatus = document.querySelector('.knapsack-status')
 const results = document.querySelector('.final-state')
 const done = document.querySelector('.btn1')
 const add = document.querySelector('.btn2')
 const restart = document.querySelector('.btn3')
+const itemList = document.querySelector('#item-list')
 let sumWeight = 0
 let sumValue = 0
 
 done.onclick = function getOption () {
   const inputVal = maxCapacity.value
+  maxCapacity.disabled = true
 
   add.addEventListener('click', () => {
     for (let i = 0; i < items.length; i++) {
@@ -42,18 +43,26 @@ done.onclick = function getOption () {
         console.log(sumWeight)
         console.log(sumValue)
         if (sumWeight <= inputVal) {
-          knapsackStatus.innerHTML +=
-            `${options.value}` +
-            ':' +
-            ' ' +
-            `weight= ${items[i].weight}` +
-            ',' +
-            ' ' +
-            `value= ${items[i].value}` +
-            '<br>'
-          results.innerHTML = `Total weight: ${sumWeight}Kg` `Total price(XAF): ${sumValue}`
+          itemList.style.backgroundColor = '#e2f4e2'
+          const li = document.createElement('li')
+          li.innerHTML = `
+            <span>${options.value}</span>
+            <span>${items[i].weight}kg</span>
+            <span>${items[i].value}fcfa</span>
+          `
+          itemList.appendChild(li)
+
+          results.innerHTML = `
+            <div>
+              <p>Total weight: ${sumWeight}</p>
+              <p>Total value(XAF): ${sumValue}</p>
+              <p>Max weight: ${inputVal}</p>
+            </div>
+          `
         } else {
           window.alert(`Exceeded maximum weight of ${inputVal}kg`)
+          done.disabled = true
+          itemList.style.backgroundColor = '#f08080'
         }
       }
     }
